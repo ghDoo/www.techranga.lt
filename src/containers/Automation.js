ea758137a3f9a03bbed3732c828c128d498d3441
ghDoo <donatas.dereskevicius@gmail.com>
2020-05-19 00:10:11 +0300

    responsive fixs

--- src/containers/Automation.js
+++ src/containers/Automation.js
@@ -10,7 +10,7 @@ export const Automation = () => {
   return (
     <div className={ `${ active ? "hide" : "automation" }` }>
       <div className="automation-content"></div>
-      <p className="automation-text"> { t(`automation.text01`) } <br /> { t(`automation.text02`) } </p>
+      <p className="automation-text"> { t(`automation.text01`) } <br /> <br /> { t(`automation.text02`) } </p>
   </div>
   );
 };
