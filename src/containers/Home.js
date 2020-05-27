91d1b5b523f79888ffe22ea66f201afe2b5e98a7
ghDoo <donatas.dereskevicius@gmail.com>
2020-05-27 23:07:43 +0300

    fixing errors and updating style

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,50 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useTranslation } from "react-i18next";
 
 import { NavigationContext } from '../App';
 
 export const Home = () => {
+  const [show, setShow] = useState(false);
   const { state } = useContext(NavigationContext);
   const active = state.isMenuActive;
   const { t } = useTranslation();
-  let slideIndex = 0;
 
-  const showSlides = () => {
-    let i;
-    const slides = document.getElementsByClassName('home-carousel');
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    };
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    };
-    for (i = 0; i < slides.length; i++) {
-      slides[slideIndex-1].style.display = "flex";
-    };
-    setTimeout(showSlides, 5000);
+  const currentState = () => {
+    if (show === false) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  showSlides();
+  const test = () => {
+    const interval = setTimeout(() => {
+      setShow(currentState);
+    }, 5000);
+    return () => clearTimeout(interval);
+  };
+
+  test();
 
   return (
     <div className={ `${ active ? "hide" : "home" }` }>
-      <div className="home-carousel">
-        <div className="home-carousel__01">
-          <h1 className="home-carousel__01_title"> { t(`home.carousel01.title`) } </h1>
-          <h2 className="home-carousel__01_text"> { t(`home.carousel01.text01`) } </h2>
-          <h2 className="home-carousel__01_text"> { t(`home.carousel01.text02`) } </h2>
-          <h2 className="home-carousel__01_text"> { t(`home.carousel01.text03`) } </h2>
-          <h2 className="home-carousel__01_text"> { t(`home.carousel01.text04`) } </h2>
-          <h2 className="home-carousel__01_text"> { t(`home.carousel01.text05`) } </h2>
+      <div className={ `${ show ? "hide" : "home-carousel" }` }>
+        <div className={ `${ show ? "hide" : "home-carousel__01" }` }>
+          <h1 className="home-carousel__01_title"> { `${ show ? "" : t(`home.carousel01.title`) }` } </h1>
+          <h2 className="home-carousel__01_text"> { `${ show ? "" : t(`home.carousel01.text01`) }` } </h2>
+          <h2 className="home-carousel__01_text"> { `${ show ? "" : t(`home.carousel01.text02`) }` } </h2>
+          <h2 className="home-carousel__01_text"> { `${ show ? "" : t(`home.carousel01.text03`) }` } </h2>
+          <h2 className="home-carousel__01_text"> { `${ show ? "" : t(`home.carousel01.text04`) }` } </h2>
+          <h2 className="home-carousel__01_text"> { `${ show ? "" : t(`home.carousel01.text05`) }` } </h2>
         </div>
       </div>
-      <div className="home-carousel">
-        <div className="home-carousel__02">
-          <h1 className="home-carousel__02_title"> { t(`home.carousel02.title`) } </h1>
-          <h2 className="home-carousel__02_text"> { t(`home.carousel02.text01`) } </h2>
-          <h2 className="home-carousel__02_text"> { t(`home.carousel02.text02`) } </h2>
-          <h2 className="home-carousel__02_text"> { t(`home.carousel02.text03`) } </h2>
+      <div className={ `${ show ? "home-carousel" : "hide" }` }>
+        <div className={ `${ show ? "home-carousel__02" : "hide" }` }>
+          <h1 className="home-carousel__02_title"> { `${ show ? t(`home.carousel02.title`) : "" }` } </h1>
+          <h2 className="home-carousel__02_text"> { `${ show ? t(`home.carousel02.text01`) : "" }` } </h2>
+          <h2 className="home-carousel__02_text"> { `${ show ? t(`home.carousel02.text02`) : "" }` } </h2>
+          <h2 className="home-carousel__02_text"> { `${ show ? t(`home.carousel02.text03`) : "" }` } </h2>
         </div>
       </div>
       <div className="home-info">
